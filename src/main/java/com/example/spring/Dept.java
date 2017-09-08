e0340c4fa29749fe50ad852b4c660ab3c1552785
java <java@bigdata>
2017-09-08 10:10:15 +0900

    lombok apply

--- src/main/java/com/example/spring/Dept.java
+++ src/main/java/com/example/spring/Dept.java
@@ -1,5 +1,16 @@
 package com.example.spring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dept {
 	int deptno;
 	String dname;
--- src/main/java/com/example/spring/DeptExample.java
+++ src/main/java/com/example/spring/DeptExample.java
@@ -9,6 +9,7 @@ public class DeptExample {
 		dept.setDname("개발부");
 		dept.setLoc("seoul");
 		
+		System.out.println(dept);
 		System.out.println(dept.getDeptno() + ", " + dept.getDname() + ", " + dept.getLoc());
 		
 	}
